d9e7effaa7732d876783cec3ae1fc7888d9dd955
KirstenWF <kirsten@wayfindergames.se>
2026-03-09 11:54:31 +0100

    linux server compile fixes

--- Source/WindowsDualsense_ds5w/Private/DeviceManager.cpp
+++ Source/WindowsDualsense_ds5w/Private/DeviceManager.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2025 Rafael Valoto/Publisher. All rights reserved.
+// Copyright (c) 2025 Rafael Valoto/Publisher. All rights reserved.
 // Created for: WindowsDualsense_ds5w - Plugin to support DualSense controller on Windows.
 // Planned Release Year: 2025
 
@@ -281,7 +281,7 @@ void DeviceManager::SetChannelValues(int32 ControllerId, const FForceFeedbackVal
 		auto GetRumbleByte = [](const float Small, const float Large) -> std::uint8_t {
 			std::uint8_t SmallNib = FMath::Clamp(Small, 0.f, 1.f) * 15;
 			std::uint8_t LargeNib = FMath::Clamp(Large, 0.f, 1.f) * 15;
-			return (LargeNib << 4) | SmallNib & 0x0f;
+			return (LargeNib << 4) | (SmallNib & 0x0f);
 		};
 
 		const std::uint8_t LeftRumble = GetRumbleByte(Values.LeftSmall, Values.LeftLarge);
